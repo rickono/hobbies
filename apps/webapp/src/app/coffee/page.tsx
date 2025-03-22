966a8b30bc768f98f6aa65f7c311ba5ece073c89
Rick Ono <rickono@Ricks-MacBook-Pro.local>
2025-03-22 09:51:22 -0500

    remove logging

--- apps/webapp/src/app/coffee/page.tsx
+++ apps/webapp/src/app/coffee/page.tsx
@@ -1,9 +1,6 @@
-import { query } from "@rono/db";
 import { Page } from "../../components/ui/page";
-import { db } from "../../lib/db";
 
 export default async function Home() {
-  console.log(await query.roaster.getAll(db!));
   return (
     <Page title="Coffee">
       <div className="w-full h-full"></div>
